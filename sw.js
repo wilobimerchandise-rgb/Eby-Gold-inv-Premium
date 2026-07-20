ae580bc3977f3a08728bca17aec6ae9833c8a56a
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-07-20 14:37:17 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'eby-gold-v3';
+const urlsToCache = [
+  '.',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
